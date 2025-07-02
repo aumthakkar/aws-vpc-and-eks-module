120bf2f0f7416ae3fbdec6355a8c5a419942f74b
aumthakkar <aumthakkar@yahoo.co.uk>
2025-07-02 17:22:49 +0100

    added the CloudWatchAgentServerPolicy IAM Policy to the eks-nodegroup-iamrole.tf

--- eks-nodegroup-iamrole.tf
+++ eks-nodegroup-iamrole.tf
@@ -49,3 +49,8 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEFSCSIDriverPolicy" {
   role       = aws_iam_role.eks_nodegroup_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "eks-CloudWatchAgentServerPolicy" {
+  role       = aws_iam_role.eks_nodegroup_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/CloudWatchAgentServerPolicy"
+}
